fe09fbca03cf164b131f43d8c2dc89fcab685e1d
윤효진 <hyojinee@dgsw.hs.kr>
2025-05-22 21:54:42 +0900

    feat: folder setting

---
+++ src/pages/singup/select-role/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './index.style';
+
+const SelectRole = () => {
+    return (
+        <S.Container>
+            <div></div>
+        </S.Container>
+    );
+};
+
+export default SelectRole;
